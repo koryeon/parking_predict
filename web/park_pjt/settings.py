417e88f28c853939323a1d5cc6d9c71a953097b6
koryeon <gydus241@gmail.com>
2025-07-23 06:52:53 +0900

    Fix static files directory for collectstatic

--- web/park_pjt/settings.py
+++ web/park_pjt/settings.py
@@ -121,8 +121,8 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / 'static']
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_DIRS = [BASE_DIR / 'web' / 'static']
+STATIC_ROOT = BASE_DIR / 'web' / 'staticfiles'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
